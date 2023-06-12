4a3cd76beb9efd08eeaeb2238ee158ba72a5fd91
luis <luis@computer.org>
2023-06-12 10:47:10 -0600

    uhmm

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
-const fs = require('fs');
-const path = require('path');
+const { app } = require("@azure/functions");
 
-const { app, output } = require('@azure/functions');
+async function status(request, context) {
+    context.log(`Http function processed request for url "${request.url}"`);
 
-app.timer('timerTrigger1', {
-    schedule: '*/5 * * * * *',
-    handler: (myTimer, context) => {
-        context.log(`Something has happened haha. Invocation ID: "${context.invocationId}"`);
-        const filePath = path.resolve(__dirname,'output.txt');
-        const data = new Date().toISOString() + '\n';
+    return {
+        status: 200,
+        jsonBody: {
+            env: process.env
+        }
+    };
+};
 
-        fs.appendFile(filePath, data, (err) => {
-            if (err) {
-                console.log(`Error occurred: ${err}`);
-            } else {
-                console.log(`Date string appended to file: ${filePath}`);
-            }
-        });
-    }
+app.http('status', {
+    route: "status",
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    handler: status
 });
+
+module.exports = status
+
+// const fs = require('fs');
+// const path = require('path');
+
+// const { app, output } = require('@azure/functions');
+
+// app.timer('timerTrigger1', {
+//     schedule: '*/5 * * * * *',
+//     handler: (myTimer, context) => {
+//         context.log(`Something has happened haha. Invocation ID: "${context.invocationId}"`);
+//         const filePath = path.resolve(__dirname,'output.txt');
+//         const data = new Date().toISOString() + '\n';
+
+//         fs.appendFile(filePath, data, (err) => {
+//             if (err) {
+//                 console.log(`Error occurred: ${err}`);
+//             } else {
+//                 console.log(`Date string appended to file: ${filePath}`);
+//             }
+//         });
+//     }
+// });
