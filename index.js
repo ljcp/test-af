f6c80306be8f422445a3a7bdd4c363b3e90c9d3a
luis <luis@computer.org>
2023-06-12 09:59:54 -0600

    file as log

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('success');
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.resolve(__dirname,'output.txt');
+const data = new Date().toISOString() + '\n';
+
+fs.appendFile(filePath, data, (err) => {
+    if (err) {
+        console.log(`Error occurred: ${err}`);
+    } else {
+        console.log(`Date string appended to file: ${filePath}`);
+    }
+});
