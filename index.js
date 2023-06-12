214e8e2c33a9c9b4bc0ece975077f43cb32cbec3
luis <luis@computer.org>
2023-06-12 10:26:06 -0600

    route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { app } = require('@azure/functions');
 
 app.http('helloWorld1', {
+    route: 'hello/world',
     methods: ['POST', 'GET'],
     handler: async (request, context) => {
         context.log('Http function was triggered.');
